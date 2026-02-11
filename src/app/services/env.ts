1e7caa7892c1d5833c68bf780f8a2478358fd91c
skypper109 <colonel.diallo19@gmail.com>
2026-02-11 14:57:01 +0000

    Commit la modification de fichier env dans angular.json

--- src/app/services/env.ts
+++ src/app/services/env.ts
@@ -1,4 +1,4 @@
-import { environment } from "../../environments/environment.prod";
+import { environment } from "../../environments/environment";
 
 export class Env {
   public static API_URL = environment.apiUrl;
